a03c9fdbf8f8f0ea27560c72d74cc7bc3ba4f4de
Abi0l9 <cryptog3ms@gmail.com>
2023-09-30 23:09:05 +0100

    installed some deps and split app into components

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Search from "./Search";
+import Main from "./Main";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <Main />
+    </div>
+  );
+};
+
+export default Layout;
